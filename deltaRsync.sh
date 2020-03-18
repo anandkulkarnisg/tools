a1337df538ac2d0e699f74f8783a51f155f40066
Anand Kulkarni <anand.kulkarni.sg@gmail.com>
2020-03-18 13:26:19 +0800

    adding succint commands for timestamp based
    adding succint commands for timestamp based

--- deltaRsync.sh
+++ deltaRsync.sh
@@ -3,18 +3,9 @@
 # Synopsis : identify files changes since a given time and sync them across via rsync.
 # Invocation : /deltaRsync.sh /home/a1384313/ak-kdb+-projects/kdb+/log4kdb /home/a1384313/rsynctest/log4kdb
 
-a1384313@uklpaumar06a[PRD][~] $ export sinceTimeStamp=$(date +'%Y-%m-%d %H:%M:%S')
-a1384313@uklpaumar06a[PRD][~] $ echo $sinceTimeStamp
-2020-03-18 03:50:07
 a1384313@uklpaumar06a[PRD][~] $ export sinceTimeStamp=$(date +'%Y-%m-%d')
-a1384313@uklpaumar06a[PRD][~] $ echo $sinceTimeStamp
-2020-03-18
 a1384313@uklpaumar06a[PRD][~] $ sinceTimeStamp=$(echo ${sinceTimeStamp}" 00:00:00")
-a1384313@uklpaumar06a[PRD][~] $ echo $sinceTimeStamp
-2020-03-18 00:00:00
 a1384313@uklpaumar06a[PRD][~] $ export execCommand="find /home/a1384313/ak-kdb+-projects/kdb+/log4kdb -newermt \"${sinceTimeStamp}\""
-a1384313@uklpaumar06a[PRD][~] $ echo ${execCommand}
-find /home/a1384313/ak-kdb+-projects/kdb+/log4kdb -newermt "2020-03-18 00:00:00"
 a1384313@uklpaumar06a[PRD][~] $ eval ${execCommand}
 /home/a1384313/ak-kdb+-projects/kdb+/log4kdb/sessionfiles
 /home/a1384313/ak-kdb+-projects/kdb+/log4kdb/sessionfiles/newsession
